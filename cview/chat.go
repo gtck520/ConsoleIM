6a95bda5845a6e33a9386928b615ce329644707b
chenkang <chenkang@yalalat.com>
2021-11-03 18:15:51 +0800

    聊天室，发送框与显示窗初步实现

--- cview/chat.go
+++ cview/chat.go
@@ -41,10 +41,16 @@ func (c *CView) Chat(nextSlide func()) (title string, content tview.Primitive) {
 	}()
 	textView.SetBorder(true).SetTitle("  消息")
 	//输入窗口
-	inputform := tview.NewForm().
-		AddInputField("请输入消息:", "", 50, nil, nil)
-
+	inputform := tview.NewForm()
+	inputform.AddInputField("请输入消息:", "", 50, nil, nil).
+		AddButton("发送", func() {
+			message := inputform.GetFormItem(0).(*tview.InputField).GetText()
+			fmt.Fprintln(textView, message)
+			inputform.GetFormItem(0).(*tview.InputField).SetText("")
+		}).
+		SetHorizontal(true)
 	inputform.SetBorder(true).SetTitle("")
+
 	//整体框架
 	flex := tview.NewFlex().
 		AddItem(tview.NewBox().SetBorder(true).SetTitle("  好友列表 "), 0, 1, false). //Left (1/2 x width of Top)
